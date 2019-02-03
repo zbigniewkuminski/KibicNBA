56d022b7b7c24b2a3023244610ad50296f47e22e
Zbigniew Kumiński <zbyszko613@gmail.com>
2019-02-03 17:28:22 +0100

    logo + open, close menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
