70be42a62fa97579aab4e6804f8a8b62cde1bea2
Zbigniew Kumiński <zbyszko613@gmail.com>
2019-04-05 23:05:33 +0200

    animations on teams screen

--- src/app/main-site/teams/teams.component.ts
+++ src/app/main-site/teams/teams.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
-  styleUrls: ['./teams.component.css']
+  styleUrls: ['./teams.component.css'],
+  animations: [
+    trigger('westConferenceTile', [
+      state('false', style({
+        'width':'480px',
+        'height':'420px'
+      })),
+      state('true', style({
+        'width':'520px',
+        'height':'440px',
+        'position':'absolute',
+      })),
+      transition('false => true', animate(600)),
+      transition('true => false', animate(300))
+    ]),
+    trigger('eastConferenceTile', [
+      state('false', style({
+        'width':'480px',
+        'height':'420px'
+      })),
+      state('true', style({
+        'width':'520px',
+        'height':'440px',
+        'position':'absolute',
+      })),
+      transition('false => true', animate(600)),
+      transition('true => false', animate(300))
+    ])
+  ]
 })
 export class TeamsComponent implements OnInit {
-
+  westHoverTile: string = 'false';
+  eastHoverTile: string = 'false';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private changeState(conference: string, state: string) {
+    if (conference == 'west') {
+      this.westHoverTile = state;
+    } else {
+      this.eastHoverTile = state;
+    }
   }
 
 }
